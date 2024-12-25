67355a016327bc6274dfa8a4f19e3f657d11f8ef
Ahmet Furkan Öcel <157826103+KayzerFurkan04@users.noreply.github.com>
2024-12-25 16:54:38 +0300

    Unity_Hafta10_Raporu

---
+++ AsteroidScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    float rotateSpeed = 20f;
+
+    public GameObject explosionPrefab;
+    public spawnmanagerscript spawnmanagerscript;
+
+    void Start()
+    {
+        spawnmanagerscript = GameObject.Find("spawnmanagerscript").GetComponent<spawnmanagerscript>();
+        if (spawnmanagerscript == null) Debug.LogError("Asteroid_sc::Start spawnmanagerscript is NULL");
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "laser")
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            spawnmanagerscript.StartSpawning();
+            Destroy(this.gameObject);
+        }
+    }
+}
